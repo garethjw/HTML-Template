f0ebf117e78d333bff57504b5cb67287debda780
gareth <gareth@blocworx.co.uk>
2010-10-28 18:15:04 +0100

    added common js and link to jQuery hosted on Google API

--- index.php
+++ index.php
@@ -1,51 +1,24 @@
-<?php
-
-include_once("includes/config.php");
-
-$CONFIG['HTML_TITLE'] = '';
-
-$CONFIG['META_KEYWORDS'] = '';
-$CONFIG['META_DESCRIPTION'] = '';
-
-
-?>
-
 <?php include("modules/page_doctype.php") ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title><?php echo $CONFIG['HTML_TITLE'] ?></title>
+<title></title>
 
-<base href="<?php echo $CONFIG['SITE_ROOT'] ?>" />
+<base href="" />
 
 <?php include("modules/page_metatags.php") ?>
 
-<style type="text/css">
-@import url('includes/default.css');
-@import url('includes/menu.css');
-</style>
+<link href="includes/default.css" rel="stylesheet" type="text/css" media="screen" />
 
-
-<!--[if IE 5]>
-<style type="text/css">
-@import url('includes/default_ie5.css');
-</style>
-<![endif]-->
-
-<script language="JavaScript" type="text/javascript" src="includes/functions.js"></script>
-<script language="JavaScript" type="text/javascript" src="includes/events_menu.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="includes/common.js"></script>
 
 </head>
 <body>
 
 <div id="divWrapper">
 
-<?php include_once("modules/page_header.php") ?>
-
-<?php include_once("modules/page_navigation.php") ?>
-
-<?php include_once("modules/page_footer.php") ?>
-
+<p></p>
 
 </div>
 
