4781f179f63964209abb5c83c918739279f27ee5
garethjw <gareth@blocworx.co.uk>
2018-10-02 14:00:16 +1000

    MTP-10 transition this issue
    #done Fixed this today

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("includes/page_doctype.php") ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>Testing branch sync - with GitHub and Jira integration</title>
+<title>Testing smart commit - done</title>
 
 <base href="" />
 
